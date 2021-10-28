81c03a8ca176a3cb75ccbd577275bfbcf660122d
Julien Hagestedt <julien.hagestedt@gmail.com>
2021-10-28 10:48:51 +0200

    fix: docker-compose installation

--- 10-package.sh
+++ 10-package.sh
@@ -2,7 +2,8 @@
 
 set -e
 
-[[ "${CONTAINER_VERSION}" ]] || (echo "ERROR: CONTAINER_VERSION variable not set." && exit 1)
+[[ "${DOCKER_VERSION}" ]] || (echo "ERROR: DOCKER_VERSION variable not set." && exit 1)
+[[ "${DOCKER_COMPOSE_VERSION}" ]] || (echo "ERROR: DOCKER_COMPOSE_VERSION variable not set." && exit 1)
 [[ "${CONTAINER_REGISTRY_PATH}" ]] || (echo "ERROR: CONTAINER_REGISTRY_PATH variable not set." && exit 1)
 
 [[ "${CONTAINER_REGISTRY_USERNAME}" ]] &&
@@ -12,11 +13,12 @@ docker login "${CONTAINER_REGISTRY_PATH}" \
 --username "${CONTAINER_REGISTRY_USERNAME}" \
 --password-stdin
 
+export CONTAINER_TAG="${DOCKER_VERSION}-docker-compose"
 docker build . \
 --build-arg DOCKER_VERSION \
 --build-arg DOCKER_COMPOSE_VERSION \
 --file Dockerfile \
---tag "${CONTAINER_REGISTRY_PATH}:${CONTAINER_VERSION}"
+--tag "${CONTAINER_REGISTRY_PATH}:${CONTAINER_TAG}"
 
 [[ "${CONTAINER_REGISTRY_PUSH}" -eq 1 ]] &&
-docker push "${CONTAINER_REGISTRY_PATH}:${CONTAINER_VERSION}"
+docker push "${CONTAINER_REGISTRY_PATH}:${CONTAINER_TAG}"
--- Dockerfile
+++ Dockerfile
@@ -1,9 +1,6 @@
 ARG DOCKER_VERSION="19.03"
-ARG DOCKER_COMPOSE_SYSTEM="Linux"
-ARG DOCKER_COMPOSE_MACHINE="x86_64"
-ARG DOCKER_COMPOSE_VERSION="1.29.2"
 FROM docker:${DOCKER_VERSION}
 RUN apk add --no-cache git curl py-pip python3-dev libffi-dev openssl-dev gcc libc-dev rust cargo make
-RUN curl --location --output /usr/local/bin/docker-compose \
-"https://github.com/docker/compose/releases/download/${DOCKER_COMPOSE_VERSION}/docker-compose-${DOCKER_COMPOSE_SYSTEM}-${DOCKER_COMPOSE_MACHINE}"
-RUN chmod +x /usr/local/bin/docker-compose
+ARG DOCKER_COMPOSE_VERSION="1.29.2"
+ENV DOCKER_COMPOSE_VERSION ${DOCKER_COMPOSE_VERSION}
+RUN pip install docker-compose==${DOCKER_COMPOSE_VERSION}
